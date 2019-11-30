d109e0d02a0b0b0924995d9229c4f5a1d02460e2
solovdaria <44465483+solovdaria@users.noreply.github.com>
2019-11-30 19:42:56 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <cmath>
+#include "ModArithmetic.h"
+#include "EllipticCurve.h"
+#include "Algorithms.h"
+
+using namespace std;
+
+int main()
+{
+	ModField f("7");
+	LongModInt x("5", &f);
+	LongModInt y("4", &f);
+
+	LongModInt result = x - y;
+	cout << x << " - " << y << " = " << result << std::endl;
+
+	result = x - y + y;
+	cout << x << " - " << y << " + " << y << " = " << result << std::endl;
+
+	result = x * y;
+	cout << x << " * " << y << " = " << result << std::endl;
+
+	result = x + y;
+	cout << x << " + " << y << " = " << result << std::endl;
+
+	result = x / y;
+	cout << x << " / " << y << " = " << result << std::endl;
+
+	result = ~y;
+	cout << y << " ^ -1 = " << result << std::endl;
+
+	result = x / y * y;
+	cout << x << " / " << y << " * " << y << " = " << result << std::endl;
+
+	bool boolResult = x < y;
+	cout << x << " < " << y << " ? " << boolalpha << boolResult << std::endl;
+
+	boolResult = x > y;
+	cout << x << " > " << y << " ? " << boolalpha << boolResult << std::endl;
+
+	boolResult = x == y;
+	cout << x << " == " << y << " ? " << boolalpha << boolResult << std::endl;
+
+	boolResult = x <= y;
+	cout << x << " <= " << y << " ? " << boolalpha << boolResult << std::endl;
+
+	result = pow(x, y);
+	cout << x << " ^ " << y << " = " << result << std::endl;
+
+	//y^2 = x^3 + 3x + 5 (mod 7)
+	ModField eField("7");
+	EllipticCurve e("3", "5", &eField);
+
+	cout << e.add({ "1", "3" }, { "1", "4" }) << endl;
+//	cout << e.add({ "6", "1" }, { "1", "4" }) << endl;
+//	cout << e.add({ "1", "4" }, { "6", "1" }) << endl;
+
+	cout << e.integerTimesPoint({ "1", "4" }, 13);
+}
