f7dff9a07eb9724a7504ee177149e4fbc831ce38
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2019-12-05 02:18:37 +0200

    Add MultiplicativeGroupModN class to represent multiplicative group of integers modulo n

---
+++ src/MultiplicativeGroupModN.cpp
@@ -0,0 +1,18 @@
+#include "MultiplicativeGroupModN.h"
+
+MultiplicativeGroupModN::MultiplicativeGroupModN(int modulo) {
+	if (modulo <= 0) {
+		throw std::invalid_argument("The modulo can not be <= 0.");
+	}
+	ModField n(std::to_string(modulo));
+	for (int i = 1; i < modulo; i++) {
+		if (gcd(i, modulo) == 1) {
+			elements.emplace_back(new LongModInt(std::to_string(i), &n));
+		}
+	}
+	order = new LongModInt(std::to_string(elements.size()), &n);
+}
+
+LongModInt* MultiplicativeGroupModN::getOrder() {
+	return order;
+}
---
+++ src/MultiplicativeGroupModN.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "ModArithmetic.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+// multiplicative group of integers modulo n - Zn* = {a from integers modulo n | gcd(a, n) = 1}
+class MultiplicativeGroupModN {
+private:
+	LongModInt* order;
+	std::vector<LongModInt*> elements;
+
+public:
+	MultiplicativeGroupModN(int modulo);
+	LongModInt* getOrder();
+};
