96ec5d68e15caeb52e0ff2f04ff61cd88f1dab27
darinamazur <43438115+darinamazur@users.noreply.github.com>
2019-12-05 02:08:07 +0200

    Add Montgomery reduction algorithm

--- src/Algorithms.h
+++ src/Algorithms.h
@@ -1,7 +1,20 @@
 #pragma once
 #include"ModArithmetic.h"
 
-LongModInt pow(const LongModInt& , const LongModInt&);
+struct binary {
+	LongModInt order;
+	int bit;
+};
+
+LongModInt pow(const LongModInt&, const LongModInt&);
+std::vector<binary> exponent(LongModInt number, int& breakpoint, int& length);
+bool isCoprime(const LongModInt& number1);
+LongModInt mont(const LongModInt& number, const LongInt& r);
+std::string convert(std::vector<int>x);
+LongInt LongModInt_to_LongInt(const LongModInt& x);
+LongInt inverse(const LongInt& x, const LongModInt& number);
+LongInt modR(const LongInt& x, const LongInt& r);
+LongModInt montgomeryMltpl(const LongModInt& number1, const LongModInt& number2, const LongInt& r, const LongInt& k, const LongInt& N);
 
 void eulerCarmichaelTest();
 bool isCarmichaelNumber(int n);
